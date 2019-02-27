2b05b1eb8379a6cc7719dda588e42c3d7cdbbd26
Max Raadgever <maxraadgever@gmail.com>
2019-02-27 10:58:23 +0100

    Changed users in dockerfile to non root

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -1,6 +1,22 @@
-#!/bin/bash
+#c!/bin/bash
+#Testing
 
-letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
+#LIVE1
+#letsencrypt certonly --webroot -w /var/www/public_html -d "$CN1" --agree-tos --email "$EMAIL" --force-renewal
+#LIVE2
+letsencrypt certonly --webroot -w /var/www/public_html -d "$CN2" --agree-tos --email "$EMAIL" --force-renewal --expand
 
-cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/cert1.pem
-cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/privkey1.pem
+
+
+#-d "$CN2" --agree-tos --email "$EMAIL" --non-interactive --text --expand
+#Live
+#letsencrypt certonly --webroot -w /var/www/public_html -d "$CN1" --agree-tos --email "$EMAIL" --non-interactive --text --expand
+mkdir /var/certs/live/"$CN1"
+cp /etc/letsencrypt/archive/"$CN1"/cert1.pem /var/certs/live/"$CN1"/cert1.pem
+cp /etc/letsencrypt/archive/"$CN1"/privkey1.pem /var/certs/live/"$CN1"/privkey1.pem
+cp /etc/letsencrypt/archive/"$CN1"/fullchain1.pem /var/certs/live/"$CN1"/fullchain1.pem
+mkdir /var/certs/live/"$CN2"
+cp /etc/letsencrypt/archive/"$CN2"/cert1.pem /var/certs/live/"$CN2"/cert1.pem
+cp /etc/letsencrypt/archive/"$CN2"/privkey1.pem /var/certs/live/"$CN2"/privkey1.pem
+cp /etc/letsencrypt/archive/"$CN2"/fullchain1.pem /var/certs/live/"$CN2"/fullchain1.pem
+
