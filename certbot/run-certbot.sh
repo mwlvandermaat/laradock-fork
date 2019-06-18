2e654ee8b050e2931280b5d25bb3a4e3e300bc6d
Michiel van der Maat <michiel.vandermaat@mijnsubsidie.com>
2019-06-18 11:35:09 +0200

    Fiix

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -1,22 +1,21 @@
 #c!/bin/bash
-#Testing
 
 #LIVE1TESTING subsidieprofiel
 letsencrypt certonly --webroot -w /var/www/public_html -d "$CN1" --agree-tos --email "$EMAIL" --force-renewal
+
 #LIVE2 mijnsubsidie
 # letsencrypt certonly --webroot -w /var/www/public_html -d "$CN2" --agree-tos --email "$EMAIL" --force-renewal --expand
 
 
 
-#-d "$CN2" --agree-tos --email "$EMAIL" --non-interactive --text --expand
 #Live
-#letsencrypt certonly --webroot -w /var/www/public_html -d "$CN1" --agree-tos --email "$EMAIL" --non-interactive --text --expand
-# mkdir /var/certs/live/"$CN1"
-# cp /etc/letsencrypt/archive/"$CN1"/cert1.pem /var/certs/live/"$CN1"/cert1.pem
-# cp /etc/letsencrypt/archive/"$CN1"/privkey1.pem /var/certs/live/"$CN1"/privkey1.pem
-# cp /etc/letsencrypt/archive/"$CN1"/fullchain1.pem /var/certs/live/"$CN1"/fullchain1.pem
-mkdir /var/certs/live/"$CN2"
-cp /etc/letsencrypt/archive/"$CN2"/cert1.pem /var/certs/live/"$CN2"/cert1.pem
-cp /etc/letsencrypt/archive/"$CN2"/privkey1.pem /var/certs/live/"$CN2"/privkey1.pem
-cp /etc/letsencrypt/archive/"$CN2"/fullchain1.pem /var/certs/live/"$CN2"/fullchain1.pem
+mkdir /var/certs/live/"$CN1"
+cp /etc/letsencrypt/archive/"$CN1"/cert1.pem /var/certs/live/"$CN1"/cert1.pem
+cp /etc/letsencrypt/archive/"$CN1"/privkey1.pem /var/certs/live/"$CN1"/privkey1.pem
+cp /etc/letsencrypt/archive/"$CN1"/fullchain1.pem /var/certs/live/"$CN1"/fullchain1.pem
+
+# mkdir /var/certs/live/"$CN2"
+# cp /etc/letsencrypt/archive/"$CN2"/cert1.pem /var/certs/live/"$CN2"/cert1.pem
+# cp /etc/letsencrypt/archive/"$CN2"/privkey1.pem /var/certs/live/"$CN2"/privkey1.pem
+# cp /etc/letsencrypt/archive/"$CN2"/fullchain1.pem /var/certs/live/"$CN2"/fullchain1.pem
 
