faa87dddfaff02d191140503153a076417b3bc0b
Michiel van der Maat <michiel.vandermaat@mijnsubsidie.com>
2019-10-25 22:33:05 +0200

    no message

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -7,7 +7,7 @@ letsencrypt certonly --webroot -w /var/www/public_html -d "$CN1" --agree-tos --e
 
 
 #Live
-# mkdir /var/certs/live/"$CN1"
+mkdir /var/certs/live/"$CN1"
 cp /etc/letsencrypt/archive/"$CN1"/cert1.pem /var/certs/live/"$CN1"/cert1.pem
 cp /etc/letsencrypt/archive/"$CN1"/privkey1.pem /var/certs/live/"$CN1"/privkey1.pem
 cp /etc/letsencrypt/archive/"$CN1"/fullchain1.pem /var/certs/live/"$CN1"/fullchain1.pem
