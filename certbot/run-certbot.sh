59ec41e774a233ba9de8293bc2b23ddf4ed93bbc
Michiel van der Maat <michiel.vandermaat@mijnsubsidie.com>
2020-01-19 15:14:08 +0100

    no message

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -2,7 +2,8 @@
 
 #LIVE1TESTING subsidieprofiel
 #You could run the command to create a certificate with “--duplicate” and “--staging” added. This would create a fake staging certificate, but save it like a real certificate. (It would be renewed two months from now and everything.)
-letsencrypt certonly --webroot -w /var/www/public_html -d "$CN1" --agree-tos --email "$EMAIL" --force-renewal 
+letsencrypt renew --webroot -w /var/www/public_html -d "$CN1" --agree-tos --email "$EMAIL" --force-renewal  --dry-run 
+# letsencrypt certonly --webroot -w /var/www/public_html -d "$CN1" --agree-tos --email "$EMAIL" --force-renewal 
 
 
 
