80ec5d9cc1d46fa3dfe29027011412ccb209dea0
Michiel van der Maat <michiel.vandermaat@mijnsubsidie.com>
2020-01-20 13:02:54 +0100

    no message

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -1,15 +1,14 @@
 #c!/bin/bash
 
-#LIVE1TESTING subsidieprofiel
-#You could run the command to create a certificate with “--duplicate” and “--staging” added. This would create a fake staging certificate, but save it like a real certificate. (It would be renewed two months from now and everything.)
+#LIVE 
+#You could run the command to create a certificate with “--dry-run and “--staging” added. This would create a fake staging certificate, but save it like a real certificate. (It would be renewed two months from now and everything.)
 letsencrypt certonly --webroot -w /var/www/public_html -d "$CN1" --agree-tos --email "$EMAIL" --force-renewal 
-# letsencrypt certonly --webroot -w /var/www/public_html -d "$CN1" --agree-tos --email "$EMAIL" --force-renewal 
 
 
 
-#Live
-mkdir /var/certs/live/"$CN1"
-cp /etc/letsencrypt/archive/"$CN1"/cert1.pem /var/certs/live/"$CN1"/cert1.pem
-cp /etc/letsencrypt/archive/"$CN1"/privkey1.pem /var/certs/live/"$CN1"/privkey1.pem
-cp /etc/letsencrypt/archive/"$CN1"/fullchain1.pem /var/certs/live/"$CN1"/fullchain1.pem
+#Live only first time on new server 
+# mkdir /var/certs/live/"$CN1"
+# cp /etc/letsencrypt/archive/"$CN1"/cert1.pem /var/certs/live/"$CN1"/cert1.pem
+# cp /etc/letsencrypt/archive/"$CN1"/privkey1.pem /var/certs/live/"$CN1"/privkey1.pem
+# cp /etc/letsencrypt/archive/"$CN1"/fullchain1.pem /var/certs/live/"$CN1"/fullchain1.pem
 
